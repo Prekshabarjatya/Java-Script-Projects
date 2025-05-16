dd1ba5b0e1e8db3d1f1fea027546fafc67d6e5b4
Preksha Barjatya <140542863+Prekshabarjatya@users.noreply.github.com>
2025-05-16 18:17:35 +0530

    Create javascript_trivia_bot.js

---
+++ javascript_trivia_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Codey";
+let botLocation = "Binary City";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is used for both front-end and back-end development.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " can run in any modern web browser without needing a compiler.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
